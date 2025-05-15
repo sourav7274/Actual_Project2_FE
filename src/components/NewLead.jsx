f3825e1fb169b0a5bafa83c9eb202b1daf4418ad
sourav7274 <samjhonson932@gmail.com>
2025-05-15 11:12:32 +0530

    slight chnages

--- src/components/NewLead.jsx
+++ src/components/NewLead.jsx
@@ -45,10 +45,10 @@ export const NewLead = () => {
 
   useEffect(() => {
     dispatch(getSaleAgents());
-  }, []);
+  }, [dispatch]);
   useEffect(() => {
     dispatch(getTags());
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
